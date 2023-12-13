4a7a57ba9707b30800308eb8f4a4b081ae4b3352
Oleg Tkach <KratosHome101@gmail.com>
2023-12-13 16:08:10 +0200

    docker

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const path = require('path')
 
 module.exports = {
-    experimental: {
-        serverActions: true,
-    },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
