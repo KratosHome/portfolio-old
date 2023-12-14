b129c6849c156476d8cc9b2681a0cd72d764be1f
Oleg Tkach <KratosHome101@gmail.com>
2023-12-14 13:16:01 +0200

    bags

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
         return [
             {
                 source: '/',
-                destination: '/',
+                destination: '/ua',
                 permanent: true,
             },
         ]
