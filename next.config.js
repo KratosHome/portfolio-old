72ee4d347fd69ee5923dc7b3d658c0bb04ed5793
Oleg Tkach <KratosHome101@gmail.com>
2023-12-14 15:34:53 +0200

    aws

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
         includePaths: [path.join(__dirname, 'styles')],
     },
     async redirects() {
-        return [
+        return [ 
             {
                 source: '/',
                 destination: '/ua',
